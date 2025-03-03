2a7e3daa4b61ec7d23f26a717d5c08ae56e08365
Katarina Milovanovic <k.milovanovic55@gmail.com>
2025-03-03 05:46:07 +0100

    CRUD operacije nad objektom knjiga

---
+++ src/controller/Controller.java
@@ -0,0 +1,89 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package controller;
+
+
+import java.util.ArrayList;
+import java.util.List;
+import model.Autor;
+import model.Knjiga;
+import model.Zanr;
+
+/**
+ *
+ * @author kmilo
+ */
+public class Controller {
+ 
+           
+    private List<Knjiga> listaKnjiga = new ArrayList<>();
+    private List<Autor> listaAutora = new ArrayList<>();
+
+    public List<Knjiga> getListaKnjiga() {
+        return listaKnjiga;
+    }
+
+    public void setListaKnjiga(List<Knjiga> listaKnjiga) {
+        this.listaKnjiga = listaKnjiga;
+    }
+
+    public List<Autor> getListaAutora() {
+        return listaAutora;
+    }
+
+    public void setListaAutora(List<Autor> listaAutora) {
+        this.listaAutora = listaAutora;
+    }
+    
+    
+    
+    private static Controller instance;
+
+    public static Controller getInstance() {
+        if(instance == null){
+            instance = new Controller();
+        }
+        return instance;
+    }
+    
+    
+
+    private Controller() {
+        
+                   
+    Autor autor1 = new Autor("Ivo", "Andric", 1897, "Biografija auora Ive Andrica");
+    Autor autor2 = new Autor("Danilo", "Kis", 1935, "Biografija auora Danila Kisa");
+    Autor autor3 = new Autor("Mesa", "Selimovic", 1910, "Biografija auora Mese Selimovica");
+
+    
+    Knjiga knjiga1 = new Knjiga("Na Drini cuprija", autor1, "123456789", 1945, Zanr.ISTORIJSKI);
+    Knjiga knjiga2 = new Knjiga("Basta, pepeo", autor2, "987654321", 1985,Zanr.DETEKTIVSKI );
+    Knjiga knjiga3 = new Knjiga("Tvrdjava", autor3, "000000000", 1954, Zanr.ROMAN);
+    
+
+    listaKnjiga.add(knjiga1);
+    listaKnjiga.add(knjiga2);
+    listaKnjiga.add(knjiga3);
+    
+    listaAutora.add(autor1);
+    listaAutora.add(autor2);
+    listaAutora.add(autor3);
+
+    
+    }
+
+    public void obrisiKnjigu(int selektovaniRed) {
+        listaKnjiga.remove(selektovaniRed);
+    }
+
+    public void dodajKnjigu(Knjiga novaKnjiga) {
+        listaKnjiga.add(novaKnjiga);
+    }
+
+  
+    
+    
+    
+}
---
+++ src/forme/ModelTabeleKnjige.java
@@ -0,0 +1,68 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package forme;
+
+import java.util.ArrayList;
+import java.util.List;
+import javax.swing.table.AbstractTableModel;
+import model.Knjiga;
+
+/**
+ *
+ * @author kmilo
+ */
+public class ModelTabeleKnjige extends AbstractTableModel {
+    
+    private List<Knjiga> listaKnjiga = new ArrayList<>();
+    private final String[] kolone = {"Naslov", "Autor", "ISBN", "Godina izdanja", "Zanr"};
+    
+    public ModelTabeleKnjige(List<Knjiga> listaKnjga){
+        this.listaKnjiga = listaKnjga;
+    }
+
+    @Override
+    public int getRowCount() {
+       return listaKnjiga.size();
+    }
+
+    @Override
+    public int getColumnCount() {
+        return kolone.length;
+    }
+
+    @Override
+    public Object getValueAt(int rowIndex, int columnIndex) {
+        Knjiga knjiga = listaKnjiga.get(rowIndex);
+        switch (columnIndex) {
+            
+            case 0:
+                return knjiga.getNaslov();
+            case 1:
+                return knjiga.getAutor().getIme() + " "+ knjiga.getAutor().getPrezime();
+            case 2:
+                return knjiga.getISBN();
+            case 3:
+                return knjiga.getGodinaIzdanja();
+            case 4:
+                return knjiga.getZanr();
+       
+            default:
+                return null;   
+        }
+    }
+
+    @Override
+    public String getColumnName(int column) {
+        return kolone[column];
+    }
+
+    void osveziPodatke() {
+        fireTableDataChanged();
+    }
+    
+    
+    
+    
+}
